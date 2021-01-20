7546c4782790df1d7518053698e219db0b6455fe
Morgan Lee <menghan.lee@ufl.edu>
2021-01-20 15:34:22 -0500

    SLAM/Localization and Navigation testing

---
+++ scripts/test_navigation.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+xterm -e " export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_ws/src/worlds/world.world; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch "
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch"
+xterm -e " export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_ws/src/worlds/world.world " &
+sleep 1
 
-#xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+
+xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "

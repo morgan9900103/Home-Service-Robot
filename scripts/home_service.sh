bc3da0a800b4455c59a6f827270f528824fb8dd7
Morgan Lee <menghan.lee@ufl.edu>
2021-01-31 18:31:01 -0500

    Fixed bugs

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -3,7 +3,7 @@
 xterm -e " export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_ws/src/worlds/world.world; roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
 
-xterm -e " export TURTLEBOT_GAZEBO_MAP_FILE=~/catkin_ws/src/map/map.yaml ;roslaunch turtlebot_gazebo amcl.launch initial_pose_x:=0.0 initial_pose_y:=0.0 initial_pose_a:=-1.5707 " &
+xterm -e " export TURTLEBOT_GAZEBO_MAP_FILE=~/catkin_ws/src/map/map.yaml ;roslaunch turtlebot_gazebo amcl_demo.launch initial_pose_x:=0.0 initial_pose_y:=0.0 initial_pose_a:=-1.5707 min_particles:=100 max_particles:=500 update_min_d:=0.1 update_min_a:=3.14/12 laser_z_hit:=0.95 laser_z_rand:=0.05 laser_likelihood_max_dist:=10.0 " &
 sleep 5
 
 xterm -e " rosrun rviz rviz -d /home/morganlee/catkin_ws/src/rvizConfig/home_service_robot.rviz " &
